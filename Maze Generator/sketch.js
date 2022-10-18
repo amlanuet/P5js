692aa050e8f9b8e0b830cda7903ff2d905a2ca72
amlanuet <teunteun@live.nl>
2022-10-18 14:19:31 +0200

    Checking Neighbors
    Checking Neighbors and if valid adding them to neighbors array

--- Maze Generator/sketch.js
+++ Maze Generator/sketch.js
@@ -1,10 +1,10 @@
 var cols, rows;
 var w = 40;
 var grid = [];
+var current;
 
 function setup() {
     // put setup code here
-    colorMode(HSB);
     createCanvas(400, 400);
     cols = floor(width / w);
     rows = floor(height / w);
@@ -15,6 +15,8 @@ function setup() {
             grid.push(cell);
         }
     }
+
+    current = grid[0];
 }
 function draw() {
     // put drawing code here
@@ -22,16 +24,49 @@ function draw() {
     for (let i = 0; i < grid.length; i++) {
         grid[i].show();
     }
+
+    current.visited = true;
+    current.checkNeighbors();
+}
+
+function index(i, j) {
+    if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+        return -1;
+    }
+    return i + j * cols;
 }
 
 function Cell(i, j) {
     this.i = i;
     this.j = j;
+    this.walls = [true, false, false, false]
+    this.visited = false;
+
+    this.checkNeighbors = function () {
+        var neighbors = [];
+
+        var top = grid[index(i, j - 1)]
+        var right = grid[index(i + 1, j)]
+        var bottom = grid[index(i, j + 1)]
+        var left = grid[index(i - 1, j)]
+
+        if (top && !top.visited) {
+            neighbors.push(top);
+        }
+        if (right && !right.visited) {
+            neighbors.push(right);
+        }
+        if (bottom && !bottom.visited) {
+            neighbors.push(bottom);
+        }
+        if (left && !left.visited) {
+            neighbors.push(left);
+        }
+    }
 
     this.show = function () {
         var x = this.i * w;
         var y = this.j * w;
-        this.walls = [true, false, false, false]
         stroke(255);
         if (this.walls[0]) {
             // line from top left of each cell to the top right of each cell
@@ -50,8 +85,10 @@ function Cell(i, j) {
             line(x, y + w, x, y);
         }
 
+        if (this.visited) {
+            fill(255, 0, 255, 100);
+            rect(x, y, w, w);
+        }
 
-        // noFill();
-        // rect(x, y, w, w);
     }
 }
