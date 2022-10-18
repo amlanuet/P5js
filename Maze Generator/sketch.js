e1441927139263d3bff2ba7e5ba095734c84c9c5
amlanuet <teunteun@live.nl>
2022-10-18 13:39:57 +0200

    Rect() --> line()
    instead of using rect function to draw a square at the X, Y and using W  to set height and width draw lines that use X + W or Y + W to set the diffrent points this will be usefull when drawing walls for the maze

--- Maze Generator/sketch.js
+++ Maze Generator/sketch.js
@@ -4,6 +4,7 @@ var grid = [];
 
 function setup() {
     // put setup code here
+    colorMode(HSB);
     createCanvas(400, 400);
     cols = floor(width / w);
     rows = floor(height / w);
@@ -30,8 +31,23 @@ function Cell(i, j) {
     this.show = function () {
         var x = this.i * w;
         var y = this.j * w;
-        stroke(255);
-        noFill();
-        rect(x, y, w, w);
+        // line from top left of each cell to the top right of each cell
+        // RED
+        stroke(1, 100, 100);
+        line(x, y, x + w, y);
+        // line from top left of each cell to the bottom left of each cell
+        // YELLOW
+        stroke(60, 100, 100);
+        line(x, y, x, y + w);
+        // line from top right of each cell to the bottom right of each cell
+        // Green
+        stroke(110, 100, 100);
+        line(x + w + 5, y, x + w + 5, y + w);
+        // line from bottom left of each cell to the bottom right of each cell
+        // BLUE
+        stroke(250, 100, 100);
+        line(x, y + w + 5, x + w, y + w + 5);
+        // noFill();
+        // rect(x, y, w, w);
     }
 }
