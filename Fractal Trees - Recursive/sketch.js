da4288b826170d3fe29ec35ea886ac80f30dc328
amlanuet <teunteun@live.nl>
2022-10-19 15:09:02 +0200

    Added leaves and Thickness
    Added leaves and made Stroke Weight based on the current level of the tree also made canvas bit bigger so you can view the tree better

--- Fractal Trees - Recursive/sketch.js
+++ Fractal Trees - Recursive/sketch.js
@@ -1,8 +1,8 @@
 var angle;
-
+var level = 100;
 var slider;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
   slider = createSlider(0, TWO_PI, PI / 4, 0.01);
 }
 
@@ -10,15 +10,16 @@ function draw() {
   background(51);
   angle = slider.value();
   stroke(255);
-  translate(200, height);
-  branch(100);
+  translate(300, height);
+  branch(175);
 }
 
 // teken de de boom
 function branch(len) {
+  strokeWeight(Math.ceil(len / 5));
   line(0, 0, 0, -len);
   translate(0, -len);
-  if (len > 4) {
+  if (len > 6) {
     push();
     rotate(angle);
     branch(len * 0.67);
@@ -26,7 +27,18 @@ function branch(len) {
     push();
     rotate(-angle);
     branch(len * 0.67);
+    if (len < 14) {
+      fill(80,random(150,255),random(0,150));
+      leaf();
+    }
     pop();
   }
   // line(0, 0, 0, -len * 0.67)
+}
+
+function leaf(){
+  noStroke();
+  ellipse(0, 0, random(6,16), 6);
+  ellipse(-15, 0, random(6,16), 6);  
+  ellipse(0, 15, random(6,16), 6);
 }
