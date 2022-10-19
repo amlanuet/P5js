abf835a89af32b956b7f74ae3229a1682a8caf8c
amlanuet <teunteun@live.nl>
2022-10-19 14:41:07 +0200

    Added recursion

--- Fractal Trees - Recursive/sketch.js
+++ Fractal Trees - Recursive/sketch.js
@@ -1,16 +1,32 @@
+var angle;
+
+var slider;
 function setup() {
   createCanvas(400, 400);
+  slider = createSlider(0, TWO_PI, PI / 4, 0.01);
 }
 
 function draw() {
   background(51);
+  angle = slider.value();
   stroke(255);
   translate(200, height);
   branch(100);
 }
 
-// teken de stam van de boom
+// teken de de boom
 function branch(len) {
   line(0, 0, 0, -len);
-
+  translate(0, -len);
+  if (len > 4) {
+    push();
+    rotate(angle);
+    branch(len * 0.67);
+    pop();
+    push();
+    rotate(-angle);
+    branch(len * 0.67);
+    pop();
+  }
+  // line(0, 0, 0, -len * 0.67)
 }
