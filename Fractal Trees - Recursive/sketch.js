3160f99c8294ed3a41ece61a1f9a1026c3fc01fc
amlanuet <teunteun@live.nl>
2022-10-19 14:21:56 +0200

    Moved to branch function

--- Fractal Trees - Recursive/sketch.js
+++ Fractal Trees - Recursive/sketch.js
@@ -4,9 +4,12 @@ function setup() {
 
 function draw() {
   background(51);
-
-  // teken de stam van de boom
-  var len = 100;
   stroke(255);
+  branch(50);
+}
+
+// teken de stam van de boom
+function branch(len) {
   line(200, height, 200, height - len);
+
 }
