2a04b7711443a84e5db06da103e2f5954bc676d3
amlanuet <teunteun@live.nl>
2022-10-19 15:32:10 +0200

    ApplyForce(gravity)
    added a function that takes a force and applies it to the objects acceleration.
    also wrote update function that can be called to update all the snowflakes

--- Snowfall/sketch.js
+++ Snowfall/sketch.js
@@ -1,14 +1,19 @@
 let snow = [];
+let gravity;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  gravity = createVector(0, 0.03);
 }
 
 function draw() {
   background(0);
   snow.push(new SnowFlake);
-  
+
   for (flake of snow) {
+    flake.applyForce(gravity);
     flake.render();
+    flake.update();
   }
 }
--- Snowfall/snowflake.js
+++ Snowfall/snowflake.js
@@ -4,13 +4,24 @@ class SnowFlake {
         let x = random(width);
         let y = random(-100, -10)
         this.pos = createVector(x, y);
-        this.vel = createVector();
+        this.vel = createVector(0, 0);
         this.acc = createVector();
+        this.r = random(4, 8);
+    }
+
+    applyForce(force){
+        this.acc.add(force);
+    }
+
+    update() {
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
     }
 
     render() {
         stroke(255);
-        strokeWeight(4);
+        strokeWeight(this.r);
         point(this.pos.x, this.pos.y);
     }
 
