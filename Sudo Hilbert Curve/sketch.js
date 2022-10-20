0c00beafb471c6ea5b2789ca9606af77d963b23b
amlanuet <teunteun@live.nl>
2022-10-20 14:24:40 +0200

    Hilbert Curve: First Iteration
    drawn the first 3 lines that can be call by the Herbert function. also for easier identification it prints the index of each point

--- Sudo Hilbert Curve/sketch.js
+++ Sudo Hilbert Curve/sketch.js
@@ -1,7 +1,42 @@
+var order = 1;
+var N = Math.pow(2, order);
+var total = N * N;
+var path = []
+
 function setup() {
-  // put setup code here
+  createCanvas(512, 512);
+  background(0);
+  for (let i = 0; i < total; i++) {
+    path[i] = hilbert(i);
+    len = width / N;
+    path[i].mult(len);
+    path[i].add(len / 2, len / 2);
+  }
 }
 
+function hilbert(i) {
+  points = [createVector(0, 0), createVector(0, 1), createVector(1, 1), createVector(1, 0)];
+  index = i & 3;
+  v = points[index];
+
+  return v;
+}
+
+let counter = 0;
 function draw() {
-  // put drawing code here
+  background(51);
+  stroke(255);
+  strokeWeight(1);
+  noFill();
+  beginShape();
+  for (let i = 0; i < path.length; i++) {
+    vertex(path[i].x, path[i].y);
+  }
+  endShape();
+
+  strokeWeight(2);
+  for (let i = 0; i < path.length; i++) {
+    point(path[i].x, path[i].y);
+    text(i, path[i].x+5, path[i].y-5);
+  }
 }
