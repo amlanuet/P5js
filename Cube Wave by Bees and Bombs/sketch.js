0e26c9d93e23c6cd49231d4f80fccb7ffb41eac6
amlanuet <teunteun@live.nl>
2022-10-21 12:01:10 +0200

    Added more rows of Oscillation rect

--- Cube Wave by Bees and Bombs/sketch.js
+++ Cube Wave by Bees and Bombs/sketch.js
@@ -10,9 +10,12 @@ function draw() {
   translate(width / 2, height / 2);
   rectMode(CENTER);
 
-  let h = map(sin(angle), -1, 1, 0, 100);
-  fill(255);
-  rect(0, 0, 10, h);
+  for (let x = 0; x < width; x+=10) {
+    let h = map(sin(angle), -1, 1, 0, 100);
+    fill(255);
+    rect(x - width / 2, 0, 10, h);
+    
+  }
 
   angle += 0.1;
 }
