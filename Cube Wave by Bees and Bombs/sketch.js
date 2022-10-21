b4cbfbe68991d48903734bb5e09536a8465b231b
amlanuet <teunteun@live.nl>
2022-10-21 12:24:22 +0200

    Moved to 3D
    moved to box instead of rect and also now using push() and pop() and translated all to boxes to you look down on it like a birds eye view

--- Cube Wave by Bees and Bombs/sketch.js
+++ Cube Wave by Bees and Bombs/sketch.js
@@ -4,7 +4,7 @@ let angleSlider;
 let offsetSlider;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400, WEBGL);
   angleSlider = createSlider(0, 2, 0.1, 0.05);
   angleSlider.position(10, height);
   angleSlider.style('width', '80px');
@@ -15,17 +15,24 @@ function setup() {
 
 function draw() {
   background(51);
-
-  translate(width / 2, height / 2);
+  translate(0, 50, -50)
+  rotateX(-PI / 8)
+  // translate(width / 2, height / 2);
   rectMode(CENTER);
+  // spin op x As
+  // rotateX(angle * 0.25)
 
   let offset = 0;
   for (let x = 0; x < width; x+=w) {
+    push();
     let a = angle + offset;
     let h = map(sin(a), -1, 1, 0, 100);
     fill(255);
-    rect(x - width / 2 + w / 2, 0, w - 2, h);
+    translate(x - width / 2, 0, 0);
+    box(w, h, w);
+    // rect(x - width / 2 + w / 2, 0, w - 2, h);
     offset += offsetSlider.value();
+    pop();
   }
 
   angle += angleSlider.value();
