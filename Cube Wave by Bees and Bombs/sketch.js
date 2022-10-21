51441c88cbe4c28bfd22a9894156db7b5f67c840
amlanuet <teunteun@live.nl>
2022-10-21 13:45:34 +0200

    DONE
    finishing touches and added some color

--- Cube Wave by Bees and Bombs/sketch.js
+++ Cube Wave by Bees and Bombs/sketch.js
@@ -1,10 +1,15 @@
 let angle = 0;
 let w = 24;
+let magicYangle;
+let oa = 350; // ortho zoom amount
+let maxD;
 let angleSlider;
 let offsetSlider;
 
 function setup() {
   createCanvas(400, 400, WEBGL);
+  magicYangle = atan(1 / sqrt(2));
+  maxD = dist(0, 0, 200, 200)
   angleSlider = createSlider(0, 2, 0.1, 0.05);
   angleSlider.position(10, height);
   angleSlider.style('width', '80px');
@@ -15,27 +20,34 @@ function setup() {
 
 function draw() {
   background(51);
-  ortho();
-  directionalLight(255, 255, 255, 0, -1, 0)
-  translate(0, 50, -50)
-  rotateX(-PI / 8)
-  // translate(width / 2, height / 2);
-  rectMode(CENTER);
+  ortho(-oa, oa, -oa, oa, -750, 750);
+  pointLight(255, 184, 90, 0, 0, 500);
+  pointLight(221, 34, 65, -500, -250, 0);
+  pointLight(79, 168, 168, 500, 250, 0);
+
+
+
+  translate(0, 50, -50);
+  rotateX(-QUARTER_PI);
+  rotateY(magicYangle);
+
   // spin op x As
-  // rotateX(angle * 0.25)
+  // rotateX(angle * 0.025)
 
-  let offset = 0;
-  for (let x = 0; x < width; x += w) {
-    push();
-    let a = angle + offset;
-    let h = map(sin(a), -1, 1, 0, 100);
-    translate(x - width / 2, 0, 0);
-    normalMaterial(255);
-    box(w - 2, h, w);
-    // rect(x - width / 2 + w / 2, 0, w - 2, h);
-    offset += offsetSlider.value();
-    pop();
+  for (let z = 0; z < height; z += w) {
+    for (let x = 0; x < width; x += w) {
+      push();
+      let d = dist(x, z, width / 2, height / 2)
+      let offset = map(d, 0, maxD, -3, 3);
+      let a = angle + offset;
+      let h = floor(map(sin(a), -1, 1, 75, 300));
+      translate(x - width / 2, 0, z - height / 2);
+      ambientMaterial(255, 255, 255);
+      // normalMaterial();
+      box(w - 2, h, w - 2);
+      // rect(x - width / 2 + w / 2, 0, w - 2, h);
+      pop();
+    }
   }
-
   angle += angleSlider.value();
 }
