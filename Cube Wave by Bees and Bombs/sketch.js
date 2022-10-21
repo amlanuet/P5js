db36e1979caf0730aa288ca6807411c0dd3a5615
amlanuet <teunteun@live.nl>
2022-10-21 12:13:18 +0200

    Applying Oscillation Done in 2D

--- Cube Wave by Bees and Bombs/sketch.js
+++ Cube Wave by Bees and Bombs/sketch.js
@@ -1,7 +1,16 @@
 let angle = 0;
+let w = 24;
+let angleSlider;
+let offsetSlider;
 
 function setup() {
   createCanvas(400, 400);
+  angleSlider = createSlider(0, 2, 0.1, 0.05);
+  angleSlider.position(10, height);
+  angleSlider.style('width', '80px');
+  offsetSlider = createSlider(0, 5, 0.1, 0.05);
+  offsetSlider.position(100, height);
+  offsetSlider.style('width', '80px');
 }
 
 function draw() {
@@ -10,12 +19,14 @@ function draw() {
   translate(width / 2, height / 2);
   rectMode(CENTER);
 
-  for (let x = 0; x < width; x+=10) {
-    let h = map(sin(angle), -1, 1, 0, 100);
+  let offset = 0;
+  for (let x = 0; x < width; x+=w) {
+    let a = angle + offset;
+    let h = map(sin(a), -1, 1, 0, 100);
     fill(255);
-    rect(x - width / 2, 0, 10, h);
-    
+    rect(x - width / 2 + w / 2, 0, w - 2, h);
+    offset += offsetSlider.value();
   }
 
-  angle += 0.1;
+  angle += angleSlider.value();
 }
