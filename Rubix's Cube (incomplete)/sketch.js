106962632e3e821abcd756455af60839a2f13d4a
amlanuet <teunteun@live.nl>
2022-10-25 12:27:46 +0200

    Representing each small cube in a Matrix

--- Rubix's Cube (incomplete)/sketch.js
+++ Rubix's Cube (incomplete)/sketch.js
@@ -18,7 +18,8 @@ const colors = [
 ];
 
 const dim = 3;
-const cube = []; // Cubie[dim][dim][dim]; initialized in setup()
+// const cube = []; // Cubie[dim][dim][dim]; initialized in setup()
+Cubie[] cube = new Cubie(dim*dim*dim)
 
 function setup() {
   // Disable the context menu on the canvas so the camera can use the right mouse button
@@ -26,17 +27,16 @@ function setup() {
 
   cam = createEasyCam({ distance: 400 });
 
-  for (let i = 0; i < dim; i++) {
+  let index = 0;
+  for (let x = -1; x <= dim; x++) {
     cube[i] = [];
-    for (let j = 0; j < dim; j++) {
+    for (let y = -1; y <= dim; y++) {
       cube[i][j] = [];
-      for (let k = 0; k < dim; k++) {
-        const len = 50;
-        const offset = (dim - 1) * len * 0.5;
-        const x = len * i - offset;
-        const y = len * j - offset;
-        const z = len * k - offset;
-        cube[i][j][k] = new Cubie(x, y, z, len);
+      for (let z = -1; z <= dim; z++) {
+        PMatrix3D matrix = new PMatrix3D();
+        matrix.translate(x, y, z)
+        cube[index] = new Cubie(matrix, len);
+        index++;
       }
     }
   }
@@ -44,11 +44,8 @@ function setup() {
 
 function draw() {
   background(51);
-  for (let i = 0; i < dim; i++) {
-    for (let j = 0; j < dim; j++) {
-      for (let k = 0; k < dim; k++) {
-        cube[i][j][k].show();
-      }
-    }
+  scale(100);
+  for (let i = 0; i < cube.length; i++) {
+     cube[i].show();
   }
 }
