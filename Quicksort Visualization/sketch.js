3f52f333852270b2d7a2afacf0aec399678c4166
amlanuet <teunteun@live.nl>
2022-11-07 10:19:15 +0100

    Starting Point
    every time you refresh the page your looking at the array unsorted 
    visualized as barrs in an 800 by 200 canvas

--- Quicksort Visualization/sketch.js
+++ Quicksort Visualization/sketch.js
@@ -1,7 +1,27 @@
+let i = 0;
+let w = 10;
+
 function setup() {
-  // put setup code here
+   createCanvas(800, 200);
+   values = new Array(floor(width / w));
+   for (let i = 0; i < values.length; i++) {
+      values[i] = random(height);
+   }
+   frameRate(5);
 }
 
 function draw() {
-  // put drawing code here
+  background(51);
+
+  for (let i = 0; i < values.length; i++) {
+     stroke(0);
+     fill(255);
+     rect(i * w, height - values[i], w, values[i]);
+  }
+}
+
+function swap(arr, a, b) {
+   let temp = arr[a];
+   arr[a] = arr[b];
+   arr[b] = temp;
 }
